e5db0f7baae7851f6e6597431625055235e44a4a
Jackwright102557058 <48079170+Jackwright102557058@users.noreply.github.com>
2019-09-17 15:00:38 +1000

    week 8

---
+++ Week_8/TicTacToe/src/app/game-piece/game-piece.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-piece',
+  templateUrl: './game-piece.component.html',
+  styleUrls: ['./game-piece.component.css']
+})
+export class GamePieceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
